7ef0f2fe2dbe1d7f262c911c9125fc375db62055
prince1096 <princeraj10121996@gmail.com>
2023-07-29 15:30:30 +0530

    div

--- src/pages/Singlevideo/Singlevideo.jsx
+++ src/pages/Singlevideo/Singlevideo.jsx
@@ -2,16 +2,43 @@ import React, { useContext } from "react";
 import { VideoContext } from "../../context/VideoProvider";
 import { useParams } from "react-router-dom";
 
+import styles from "./Singlevideo.module.css";
+
+import { AiFillYoutube } from "react-icons/ai";
+
 const Singlevideo = () => {
   const { state } = useContext(VideoContext);
 
   const { videoid } = useParams();
 
-  const findVideo = state?.allvideos?.find((video) => video?._id === +videoid);
+  const finalVideo = state?.allvideos?.find((video) => video?._id === +videoid);
 
-  console.log(findVideo);
+  console.log(finalVideo);
 
-  return <div></div>;
+  return (
+    <div>
+      {/* first part */}
+      <div>
+        <div className={styles.imgdiv}>
+          <img
+            src={finalVideo?.thumbnail}
+            alt=""
+            width="800px"
+            height="600px"
+          />
+
+          {/* <a href={finalVideo?.src}>ok</a> */}
+
+          <button className={styles.play}>
+            {" "}
+            <a href={finalVideo?.src}>
+              <AiFillYoutube />{" "}
+            </a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Singlevideo;
